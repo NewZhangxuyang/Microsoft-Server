a0db877040cd38a451f4b0ef8ad588e831c794e9
zhangxuyang <zhnagxuyang@wisdytech.cn>
2022-08-30 18:09:43 +0800

    引入GateWay依赖，构建GateWay子项目

---
+++ cloud-demo/GateWay/src/main/java/cn/wzd/gateway/GateWayApplication.java
@@ -0,0 +1,16 @@
+package cn.wzd.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : zhangxuyang
+ * @date :  2022/8/30
+ * @Description :
+ */
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class);
+    }
+}
